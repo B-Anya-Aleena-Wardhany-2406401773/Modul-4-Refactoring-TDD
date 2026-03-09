7acbced67bab701045f98f206def536ec15e2122
AnyaAleena <anya.aleenaa@gmail.com>
2026-03-09 11:14:45 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {

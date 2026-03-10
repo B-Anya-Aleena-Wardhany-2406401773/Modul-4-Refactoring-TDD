5ad070e8ecdc16be9cd7c031fe15bb9ae8b9c3ec
AnyaAleena <anya.aleenaa@gmail.com>
2026-03-10 19:40:30 +0700

    [REFACTOR] Apply Order and Payment integration to tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceTest.java
@@ -2,7 +2,9 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import id.ac.ui.cs.advprog.eshop.service.validator.CashOnDeliveryValidator;
 import id.ac.ui.cs.advprog.eshop.service.validator.PaymentValidator;
@@ -34,6 +36,7 @@ class PaymentServiceTest {
     PaymentRepository paymentRepository;
 
     List<Payment> payments;
+    List<Order> orders;
 
     @BeforeEach
     void setUp() {
@@ -43,16 +46,38 @@ class PaymentServiceTest {
 
         ReflectionTestUtils.setField(paymentService, "paymentRepository", paymentRepository);
 
+
+        List<Product> products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+        products.add(product1);
+
+        orders = new ArrayList<>();
+        Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                products, 1708560000L, "Safira Sudrajat");
+        orders.add(order1);
+
+        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
+                products, 1708570000L, "Safira Sudrajat");
+        orders.add(order2);
+
+        Order order3 = new Order("f47ac10b-58cc-4372-a567-0e02b2c3d479",
+                products, 1708580000L, "Safira Sudrajat");
+        orders.add(order3);
+
+
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
 
         payments = new ArrayList<>();
 
-        Payment payment1 = new Payment("13652556-012a-4c07-b546-54eb1396d79b",
+        Payment payment1 = new Payment("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
                 "VOUCHER", paymentData);
         payments.add(payment1);
 
-        Payment payment2 = new Payment("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
+        Payment payment2 = new Payment(orders.get(0).getId(),
                 "VOUCHER", paymentData);
         payments.add(payment2);
     }
@@ -60,10 +85,10 @@ class PaymentServiceTest {
     @Test
     void testCreatePayment() {
         Payment payment = payments.get(1);
-        doReturn(payment).when(paymentRepository).save(payment);
+        doReturn(payment).when(paymentRepository).save(any(Payment.class));
 
-        Payment result = paymentService.createPayment(payment);
-        verify(paymentRepository, times(1)).save(payment);
+        Payment result = paymentService.createPayment(orders.get(0), payment.getMethod(), payment.getPaymentData());
+        verify(paymentRepository, times(1)).save(any(Payment.class));
         assertEquals(payment.getId(), result.getId());
     }
 
@@ -72,8 +97,8 @@ class PaymentServiceTest {
         Payment payment = payments.get(1);
         doReturn(payment).when(paymentRepository).findById(payment.getId());
 
-        assertNull(paymentService.createPayment(payment));
-        verify(paymentRepository, times(0)).save(payment);
+        assertNull(paymentService.createPayment(orders.get(0), payment.getMethod(), payment.getPaymentData()));
+        verify(paymentRepository, times(0)).save(any(Payment.class));
     }
 
     @Test
@@ -141,9 +166,8 @@ class PaymentServiceTest {
     void testCreatePaymentVoucherInvalidCode() {
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ABC12345");
-        Payment invalidCodePayment = new Payment("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
-                PaymentMethod.VOUCHER.getValue(), paymentData);
-        paymentService.createPayment(invalidCodePayment);
+
+        Payment invalidCodePayment = paymentService.createPayment(orders.get(1), PaymentMethod.VOUCHER.getValue(), paymentData);
 
         assertEquals(invalidCodePayment.getStatus(), PaymentStatus.REJECTED.getValue());
     }
@@ -152,10 +176,8 @@ class PaymentServiceTest {
     void testCreatePaymentCODNullAddress() {
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("address", null);
-        Payment nullAddressPayment = new Payment("7f9e15bb-4b15-42f4-aebc-c3af385fb078",
-                PaymentMethod.CASH_ON_DELIVERY.getValue(), paymentData);
 
-        paymentService.createPayment(nullAddressPayment);
+        Payment nullAddressPayment = paymentService.createPayment(orders.get(2), PaymentMethod.CASH_ON_DELIVERY.getValue(), paymentData);
 
         assertEquals(nullAddressPayment.getStatus(), PaymentStatus.REJECTED.getValue());
     }
